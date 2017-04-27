cab1da5d92cd2ce7db5e197eedc4255321ea319b
Kristian Engberg <krissoengberg@gmail.com>
2017-04-27 15:02:02 +0200

    formatting

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -3,51 +3,42 @@
 class Card
 {
   public:
-  bool isInside = false;
-  char *pinInChar;
-  int *pins;
-  int amountOfPins;
- 
+    bool isInside = false;
+    char *pinInChar;
+    int *pins;
+    int amountOfPins;
+
     Card(int howManyPins)
-  {
-    pins = new int[howManyPins] ;
-    pinInChar = new char[howManyPins];
-    this->amountOfPins = (int)howManyPins;
-    
-    for(int i = 0; i < howManyPins; i++)
     {
-    pinMode(i+2, INPUT_PULLUP);
-    
-    this->pins[i] = i+2;
-    this->pinInChar[i] = '0' + pins[i];
-     
-   
-    
-    }
-    
-  }
-  
-  
+      pins = new int[howManyPins] ;
+      pinInChar = new char[howManyPins];
+      this->amountOfPins = (int)howManyPins;
 
+      for (int i = 0; i < howManyPins; i++)
+      {
+        pinMode(i + 2, INPUT_PULLUP);
 
-  void checkStatus()
-  {
-    for(int i = 0; i < amountOfPins; i++)
-    {
-    
-    if(digitalRead(pins[i]) == 0)
-    {
-      Serial.write(pins[i]);
+        this->pins[i] = i + 2;
+        this->pinInChar[i] = '0' + pins[i];
+
+      }
+
     }
-    else
+
+    void checkStatus()
     {
+      for (int i = 0; i < amountOfPins; i++)
+      {
+
+        if (digitalRead(pins[i]) == 0)
+        {
+          Serial.write(pins[i]);
+          break;
+        }
+
+      }
       Serial.write(0);
     }
-    
-    }
-
-    
-  }
 
 };
 
@@ -58,16 +49,16 @@ Card *cards;
 void setup() {
   // put your setup code here, to run once:
 
-int amountOfCards = 12;
-  
-Serial.begin(9600);
-cards = new Card(amountOfCards);
+  int amountOfCards = 12;
+
+  Serial.begin(9600);
+  cards = new Card(amountOfCards);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-cards->checkStatus();
+  cards->checkStatus();
 
 }
 
