9fd5b69eaeb10a1d363239a839dbcbaaebbf9c9e
Kristianengberg <krissoengberg@gmail.com>
2017-05-10 21:56:56 +0200

    added possible analog code

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,76 +1,87 @@
-
 class Card
 {
   public:
     bool isInside = false;
     char *pinInChar;
     int *pins;
+    int *aPins;
     bool *beenPlayed;
+    bool *analogBeenPlayed;
     int amountOfPins;
+    int amountOfAPins;
 
-    Card(int howManyPins)
+    Card(int howManyPins, int howManyAPins)
     {
-      pins = new int[howManyPins] ;
+      pins = new int[howManyPins];
+      aPins = new int[howManyAPins];
       pinInChar = new char[howManyPins];
       beenPlayed = new bool[howManyPins];
+      analogBeenPlayed = new bool[howManyAPins * 3];
       this->amountOfPins = (int)howManyPins;
-      
+      this->amountOfAPins = (int)howManyAPins;
 
       for (int i = 0; i < howManyPins; i++)
       {
-        pinMode(i +2, INPUT_PULLUP);
+        pinMode(i + 2, INPUT_PULLUP);
 
-        this->pins[i] = i +2;
+        this->pins[i] = i + 2;
         this->pinInChar[i] = '0' + pins[i];
         this->beenPlayed[i] = {false};
-
       }
 
+      for (int i = 0; i < howManyAPins * 3; i++)
+      {
+        this->analogBeenPlayed[i] = {false};
+      }
     }
 
     void checkStatus()
     {
       for (int i = 0; i < amountOfPins; i++)
       {
-
         if (digitalRead(pins[i]) == 1 && beenPlayed[i] == false)
         {
           Serial.write(pins[i]);
           Serial.println(pins[i]);
           beenPlayed[i] = true;
-          
-          break;
         }
-        
-
       }
-      Serial.write(0);
+      for (int i = 0; i < amountOfAPins; i++)
+      {
+        if (analogRead(i) < 510 && analogBeenPlayed[i] == false)
+        {
+          Serial.println("This is analog read 1");
+        }
+        else if (analogRead(i) > 550 && analogBeenPlayed[i + 1] == false)
+        {
+          Serial.println("This is analog read 2");
+        }
+        else if (analogRead(i) > 0 && analogBeenPlayed[i + 2] == false)
+        {
+          Serial.println("This is analog read 3");
+        }
+      }
     }
-    
-
 };
-
 
 Card *cards;
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-
-  int amountOfCards = 3;
+  int digitalCards = 11;
+  int analogCards = 4;
 
   Serial.begin(9600);
-  
-  Serial.println("Setup Complete");
-  cards = new Card(amountOfCards);
-  
 
+  Serial.println("Setup Complete");
+  cards = new Card(digitalCards, analogCards);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   cards->checkStatus();
-
 }
 
 
